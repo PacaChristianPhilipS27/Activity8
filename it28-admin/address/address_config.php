5f065f313b16b4bfb023b6daf5255d8b974e7695
PacaChristianPhilipS27 <cybercion.xtian@gmail.com>
2024-06-04 08:55:55 -0300

    Added Files First Commit

---
+++ it28-admin/address/address_config.php
@@ -0,0 +1,21 @@
+<?php
+// Database configuration
+
+$dbHost = 'localhost';
+$dbName = 'paca';
+$dbUsername = 'root';
+$dbPassword = '';
+/*
+$host = 'localhost';
+$dbname = 'u593341949_db_paca';
+$username = 'u593341949_dev_paca';
+$password = 'Paca';
+*/
+try {
+    $pdo = new PDO("mysql:host={$dbHost};dbname={$dbName}", $dbUsername, $dbPassword);
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
---
+++ it28-admin/address/config.php
@@ -0,0 +1,22 @@
+<?php
+// config.php
+
+// $host = 'localhost';
+// $dbname = 'paca';
+// $username = 'root';
+// $password = '';
+
+$host = 'localhost';
+$dbname = 'u593341949_db_paca';
+$username = 'u593341949_dev_paca';
+$password = 'Paca';
+
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
+}
+?>
